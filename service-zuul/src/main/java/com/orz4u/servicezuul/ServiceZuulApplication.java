9a876d808cd785b78003fa7f71b1689759cc927e
Jason Wang <vicente900211@gmail.com>
2018-03-03 21:53:30 +0800

    update

---
+++ service-zuul/src/main/java/com/orz4u/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,16 @@
+package com.orz4u.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ServiceZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZuulApplication.class, args);
+	}
+}
